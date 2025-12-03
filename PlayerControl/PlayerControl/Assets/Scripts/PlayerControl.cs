eb45b8577c75772a66cf7b612db8faca8370f817
Carter Wadlington <wadlingtonc@roverkids.org>
2025-12-03 13:12:45 -0500

    1.4

--- PlayerControl/PlayerControl/Assets/Scripts/PlayerControl.cs
+++ PlayerControl/PlayerControl/Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
+ 
 {
     // Start is called before the first frame update
     void Start()
@@ -11,10 +12,20 @@ public class PlayerControl : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float speed = 20.0f;
+private float horizontalInput;
+private float turnSpeed = 45.0f;
+private float forwardInput;
 
-    public float speed = 11;
-    void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    
+    void Update() {
+
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
